9338dbc6a28ac2a94d87c8ccae6b07226ea52a95
Rob Benz <benz_rob@yahoo.com>
2017-01-12 08:44:12 -0500

    loaded front end contant WAY smarter -- dynmaically product tab content based on tab count - and dynamic callback function

--- wp-content/plugins/dia-wc-product-tabs/dia-tabs-frontend.php
+++ wp-content/plugins/dia-wc-product-tabs/dia-tabs-frontend.php
@@ -3,7 +3,7 @@
 
 add_filter( 'woocommerce_product_tabs', 'dia_new_product_tabs' );
 
-/*** ADD TABS ***/
+/*** ADD TABS & TAB TITLES ***/
 function dia_new_product_tabs( $tabs ) {
   global $post;
   $dia_tab_count = get_post_meta( $post->ID, '_wcj_custom_product_tabs_local_total_number', true );
@@ -15,9 +15,10 @@ function dia_new_product_tabs( $tabs ) {
 
     if ( strlen($dia_tab_title) > 0 && strlen($dia_tab_content) > 0 ) {
       $tabs[$dia_tab_title_clean] = array(
-        'title' 	=> __( $dia_tab_title, 'woocommerce' ),
+        'title'   	=> __( $dia_tab_title, 'woocommerce' ),
         'priority' 	=> $y+50,
-        'callback' 	=> 'dia_new_product_tab_content'.$y
+        'callback' 	=> 'dia_new_product_tab_content',
+        'args'      => '_wcj_custom_product_tabs_content_local_' . $y
       );
     } // end foreach
   }
@@ -25,45 +26,11 @@ function dia_new_product_tabs( $tabs ) {
 }
 /*** END ***/
 
-/*** CALL BACK FOR TAB CONTENT PER DB ***/
-/*** MAKE THIS SMARTER ***/
-function dia_new_product_tab_content1() {
+/*** DYNAMICALLY LOAD TAB CONTENT ***/
+function dia_new_product_tab_content($param, $args) {
   global $post;
-   $dia_tab_content = get_post_meta( $post->ID, "_wcj_custom_product_tabs_content_local_1", true );
-   if (strlen($dia_tab_content) > 0) {
+  $table = end($args);
+  $dia_tab_content = get_post_meta( $post->ID, $table, true );
      echo $dia_tab_content;
-   }
-}
-
-function dia_new_product_tab_content2() {
-  global $post;
-   $dia_tab_content = get_post_meta( $post->ID, "_wcj_custom_product_tabs_content_local_2", true );
-   if (strlen($dia_tab_content) > 0) {
-     echo $dia_tab_content;
-   }
-}
-
-function dia_new_product_tab_content3() {
-  global $post;
-   $dia_tab_content = get_post_meta( $post->ID, "_wcj_custom_product_tabs_content_local_3", true );
-   if (strlen($dia_tab_content) > 0) {
-     echo $dia_tab_content;
-   }
-}
-
-function dia_new_product_tab_content4() {
-  global $post;
-   $dia_tab_content = get_post_meta( $post->ID, "_wcj_custom_product_tabs_content_local_4", true );
-   if (strlen($dia_tab_content) > 0) {
-     echo $dia_tab_content;
-   }
-}
-
-function dia_new_product_tab_content5() {
-  global $post;
-   $dia_tab_content = get_post_meta( $post->ID, "_wcj_custom_product_tabs_content_local_5", true );
-   if (strlen($dia_tab_content) > 0) {
-     echo $dia_tab_content;
-   }
 }
 /*** END ***/
