8c3cedfb243d6f0f26075dfc2cf380b090a54cb2
Benzy25 <benz_rob@yahoo.com>
2017-01-17 19:29:47 -0500

    renamed files with new prefix, and updated core file as needed

---
+++ benz-wc-product-tabs.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: WooCommerce Product Tabs
+Plugin URI: robbenz.com
+Description:  This plugin will add as many product tabs to your woocommerce product pages as needed. 
+Version: 1.0
+Author: Rob Benz
+Author URI: robbenz.com
+License: GPL2
+*/
+
+
+include( plugin_dir_path( __FILE__ ) . 'benz-tabs-admin.php');
+include( plugin_dir_path( __FILE__ ) . 'benz-tabs-frontend.php');
