8f081a9735685f9e4df95128e501ce8d5e4071fa
Benzy25 <benz_rob@yahoo.com>
2017-01-19 23:04:15 -0500

    begin work on plugin settings so user can toggle max about of tabs - and default number of tabs from an admin page.

--- benz-wc-product-tabs.php
+++ benz-wc-product-tabs.php
@@ -29,7 +29,78 @@ function benz_tabs_plugin_notice(){
 add_action( 'admin_init', 'benz_tabs_includes' );
 function benz_tabs_includes() {
   if ( is_plugin_active( plugin_basename( __FILE__ ) ) ) {
-    include( plugin_dir_path( __FILE__ ) . 'benz-tabs-admin.php');
-    include( plugin_dir_path( __FILE__ ) . 'benz-tabs-frontend.php');
+    $mypluginrequires = array(
+      'benz-tabs-admin.php',
+    //  'benz-tabs-admin-settings.php',
+      'benz-tabs-frontend.php'
+    );
+    foreach ( $mypluginrequires as $need ) {
+      include( plugin_dir_path( __FILE__ ) . $need );
+    }
   }
 }
+
+//add admin settings
+add_action('admin_init', 'benz_admin_init');
+add_action('admin_menu', 'benz_plugin_menu' );
+
+/**
+ * Add plugin admin settings
+ */
+function benz_admin_init() {
+    register_setting('benz-group', 'benz_dashboard_title');
+    register_setting('benz-group', 'benz_number_of_items');
+}
+
+/**
+ * add menu to admin
+ */
+function benz_plugin_menu() {
+    $blog_title = get_bloginfo( 'name' );
+    add_options_page( "$blog_title Custom Product Tabs Options", "$blog_title Custom Product Tabs", 'manage_options', 'benz', 'benz_plugin_options' );
+}
+
+/**
+ * show admin settings page
+ */
+function benz_plugin_options() {
+  $blog_title = get_bloginfo( 'name' );
+    ?>
+    <div class="wrap">
+        <?php screen_icon(); ?>
+        <h2><?php echo $blog_title; ?> Custom Product Tabs Options</h2>
+        <form action="options.php" method="post">
+            <?php settings_fields('benz-group'); ?>
+            <?php @do_settings_fields('benz-group'); ?>
+            <table class="form-table">
+                <tr valign="top">
+                    <th scope="row"><label for="benz_dashboard_title">Default Number of Tabs</label></th>
+                    <td>
+                        <input type="text" name="benz_dashboard_title" id="dashboard_title" value="<?php echo get_option('benz_dashboard_title'); //here ?>" />
+                        <br/><small>Default Number of Tabs</small>
+                    </td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row"><label for="benz_number_of_items">Max Number of Tabs</label></th>
+                    <td>
+                        <input type="text" name="benz_number_of_items" id="dashboard_title" value="<?php echo get_option('benz_number_of_items'); // here ?>" />
+                        <br/><small>Max Number of Tabs</small>
+                    </td>
+                </tr>
+
+                <tr valign="top">
+                    <th scope="row"><label for="XX">Kill</label></th>
+                    <td>
+                        <input type="checkbox" name="XX" id="Kill" value="" />
+                        <br/><small>Want to delete all the data when you un-install? Please only check if you know what you are doing. <br>If this box is checked when you uninstall or delete ( not deactivate ) this plugin <br><strong>All of your tab data will be lost.</strong></small>
+                    </td>
+                </tr>
+
+
+            </table> <?php @submit_button(); ?>
+        </form>
+    </div>
+    <?php
+}
+
+ ?>
